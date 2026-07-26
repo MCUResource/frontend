5b4da812bced6c52533dd7e03f0a14b2a206ac71
Rachel Smiley <rachelksmiley@outlook.com>
2026-07-26 18:46:49 -0400

    added login scripts

---
+++ scripts/login.js
@@ -0,0 +1,41 @@
+let token
+
+document.querySelector("#loginBtn").addEventListener("click",async function(){
+    const username = document.querySelector("#username").value
+    const password = document.querySelector("#password").value
+    login(username,password)
+})
+
+async function login(username,password){
+    const login_cred = {
+        username,
+        password
+    }
+    //send the login post request to the backend
+    const response = await fetch("https://backend-meq1.onrender.com/api/auth/",{
+        method: "POST",
+        headers{
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify(login_cred)
+    })
+
+    if(response.ok){
+        //take the token and save it to storage
+        const tokenResponse = await response.json()
+        token = tokenResponse.token
+        uname = tokenResponse.username2
+        auth = tokenResponse.auth
+        console.log(token)
+
+        //save it
+        localStorage.setItem("token", token)
+        localStorage.setItem("uname", uname)
+        localStorage.setItem("auth", auth)
+        //redirect
+        window.location.replace("/index.html")S
+    }
+    else{
+        document.querySelector("errorMsg").innerHTML = "Bad username and password"
+    }
+}
