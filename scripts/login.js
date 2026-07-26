738e2581607fa88db137fd3b6ccd46bedf4ee560
Rachel Smiley <rachelksmiley@outlook.com>
2026-07-26 19:06:48 -0400

    fixed

--- scripts/login.js
+++ scripts/login.js
@@ -34,9 +34,9 @@ async function login(username,password){
         localStorage.setItem("uname", uname)
         localStorage.setItem("auth", auth)
         //redirect
-        window.location.replace("/index.html")
+        window.location.replace("index.html")
     }
     else{
-        document.querySelector("errorMsg").innerHTML = "Bad username and password"
+        document.querySelector("#errorMsg").innerHTML = "Bad username and password"
     }
 }
